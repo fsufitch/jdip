f8d2ed82276a1dcff5101a939e63ef4adc7e4eb3
Jiri Peinlich <jiri.peinlich@gmail.com>
2018-12-01 22:29:44 +0000

    adding two cases for when one of the supports is owned by austria (independent of russia or turkey)

--- jdip_168r1/src/test/java/info/jdip/regression/Issue147.java
+++ jdip_168r1/src/test/java/info/jdip/regression/Issue147.java
@@ -11,6 +11,7 @@ import static info.jdip.test.builder.standard.StandardLocation.ANKARA;
 import static info.jdip.test.builder.standard.StandardLocation.BULGARIA;
 import static info.jdip.test.builder.standard.StandardLocation.CONSTANTINOPLE;
 import static info.jdip.test.builder.standard.StandardLocation.GREECE;
+import static info.jdip.test.builder.standard.StandardPower.AUSTRIA;
 import static info.jdip.test.builder.standard.StandardPower.RUSSIA;
 import static info.jdip.test.builder.standard.StandardPower.TURKEY;
 import static info.jdip.world.Phase.PhaseType.MOVEMENT;
@@ -19,8 +20,8 @@ import static info.jdip.world.Phase.SeasonType.SPRING;
 public class Issue147 {
     @Test
     @Disabled
-    @DisplayName("Issue 147: should not create a convoy paradox")
-    public void shouldNotCreateParadox() throws Exception {
+    @DisplayName("Issue 147: should not create a convoy paradox. 2v2 should bounce")
+    public void shouldNotCreateParadoxTwoVersusTwoShouldBounce() throws Exception {
         Case testCase = standard(SPRING, 1901, MOVEMENT)
                 .army(RUSSIA, BULGARIA)
                 .army(RUSSIA, GREECE)
@@ -75,6 +76,50 @@ public class Issue147 {
                 .expectArmy(RUSSIA, BULGARIA)
                 .expectArmy(RUSSIA, GREECE)
                 .expectArmy(RUSSIA, ANKARA)
+                .expectArmy(TURKEY, CONSTANTINOPLE)
+                .build();
+
+        TestCaseRunner.runCase(testCase);
+    }
+
+
+    @Test
+    @DisplayName("Issue 147: should not create a convoy paradox if one supporting unit is from austria")
+    public void shouldNotCreateParadoxWithAustriaOwningOneUnit() throws Exception {
+        Case testCase = standard(SPRING, 1901, MOVEMENT)
+                .army(RUSSIA, BULGARIA)
+                .army(AUSTRIA, GREECE)
+                .army(RUSSIA, ANKARA)
+                .army(TURKEY, CONSTANTINOPLE)
+                .order(CONSTANTINOPLE).moveTo(BULGARIA)
+                .order(BULGARIA).moveTo(CONSTANTINOPLE)
+                .order(GREECE).supportMove(CONSTANTINOPLE, BULGARIA)
+                .order(ANKARA).supportMove(BULGARIA, CONSTANTINOPLE)
+                .expectArmy(RUSSIA, BULGARIA)
+                .expectArmy(AUSTRIA, GREECE)
+                .expectArmy(RUSSIA, ANKARA)
+                .expectArmy(TURKEY, CONSTANTINOPLE)
+                .build();
+
+        TestCaseRunner.runCase(testCase);
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Issue 147: should not create a convoy paradox if the other supporting unit is from austria")
+    public void shouldNotCreateParadoxWithAustriaOwningOtherUnit() throws Exception {
+        Case testCase = standard(SPRING, 1901, MOVEMENT)
+                .army(RUSSIA, BULGARIA)
+                .army(RUSSIA, GREECE)
+                .army(AUSTRIA, ANKARA)
+                .army(TURKEY, CONSTANTINOPLE)
+                .order(CONSTANTINOPLE).moveTo(BULGARIA)
+                .order(BULGARIA).moveTo(CONSTANTINOPLE)
+                .order(GREECE).supportMove(CONSTANTINOPLE, BULGARIA)
+                .order(ANKARA).supportMove(BULGARIA, CONSTANTINOPLE)
+                .expectArmy(RUSSIA, BULGARIA)
+                .expectArmy(RUSSIA, GREECE)
+                .expectArmy(AUSTRIA, ANKARA)
                 .expectArmy(TURKEY, CONSTANTINOPLE)
                 .build();
 
