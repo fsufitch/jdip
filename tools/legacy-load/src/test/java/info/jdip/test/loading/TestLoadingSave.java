157fde7455255250530095f5895dbb1fe184aaf2
Jiri Peinlich <jiri.peinlich@gmail.com>
2018-12-09 17:46:30 +0000

    adding more testing files

--- tools/legacy-load/src/test/java/info/jdip/test/loading/TestLoadingSave.java
+++ tools/legacy-load/src/test/java/info/jdip/test/loading/TestLoadingSave.java
@@ -2,6 +2,7 @@ package info.jdip.test.loading;
 
 import dip.world.World;
 import dip.world.variant.VariantManager;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -18,12 +19,81 @@ public class TestLoadingSave {
         info.jdip.world.variant.VariantManager.init(new File[]{file},false);
     }
 
+    @Ignore
     @Test
-    public void loadSave() throws Exception {
-        InputStream in = this.getClass().getClassLoader().getResourceAsStream("info/jdip/legacy/saves/bug_147.jdip");
+    public void load_1902_winter() throws Exception {
+        String name = "info/jdip/legacy/saves/1902_winter.jdip";
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream(name);
         World legacyWorld = LegacyWorldLoader.readGameFile(in);
-        in = this.getClass().getClassLoader().getResourceAsStream("info/jdip/legacy/saves/bug_147.jdip");
+        in = this.getClass().getClassLoader().getResourceAsStream(name);
         boolean wordsAreEqual = WorldComparator.compareWorlds(legacyWorld, NewWorldLoader.importGameFile(in));
         assertTrue(wordsAreEqual);
     }
+
+    @Ignore
+    @Test
+    public void load_bug_147() throws Exception {
+        String name = "info/jdip/legacy/saves/bug_147.jdip";
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream(name);
+        World legacyWorld = LegacyWorldLoader.readGameFile(in);
+        in = this.getClass().getClassLoader().getResourceAsStream(name);
+        boolean wordsAreEqual = WorldComparator.compareWorlds(legacyWorld, NewWorldLoader.importGameFile(in));
+        assertTrue(wordsAreEqual);
+    }
+
+    @Ignore
+    @Test
+    public void load_1905_summer_before_retreat() throws Exception {
+        String name = "info/jdip/legacy/saves/1905-summer-before-retreat.jdip";
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream(name);
+        World legacyWorld = LegacyWorldLoader.readGameFile(in);
+        in = this.getClass().getClassLoader().getResourceAsStream(name);
+        boolean wordsAreEqual = WorldComparator.compareWorlds(legacyWorld, NewWorldLoader.importGameFile(in));
+        assertTrue(wordsAreEqual);
+    }
+
+    @Ignore
+    @Test
+    public void load_1912_fall() throws Exception {
+        String name = "info/jdip/legacy/saves/1912-fall.jdip";
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream(name);
+        World legacyWorld = LegacyWorldLoader.readGameFile(in);
+        in = this.getClass().getClassLoader().getResourceAsStream(name);
+        boolean wordsAreEqual = WorldComparator.compareWorlds(legacyWorld, NewWorldLoader.importGameFile(in));
+        assertTrue(wordsAreEqual);
+    }
+
+    @Ignore
+    @Test
+    public void load_1913_spring() throws Exception {
+        String name = "info/jdip/legacy/saves/1913-spring.jdip";
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream(name);
+        World legacyWorld = LegacyWorldLoader.readGameFile(in);
+        in = this.getClass().getClassLoader().getResourceAsStream(name);
+        boolean wordsAreEqual = WorldComparator.compareWorlds(legacyWorld, NewWorldLoader.importGameFile(in));
+        assertTrue(wordsAreEqual);
+    }
+
+    @Ignore
+    @Test
+    public void load_Loeb9_F2F() throws Exception {
+        String name = "info/jdip/legacy/saves/Loeb9-F2F.jdip";
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream(name);
+        World legacyWorld = LegacyWorldLoader.readGameFile(in);
+        in = this.getClass().getClassLoader().getResourceAsStream(name);
+        boolean wordsAreEqual = WorldComparator.compareWorlds(legacyWorld, NewWorldLoader.importGameFile(in));
+        assertTrue(wordsAreEqual);
+    }
+
+    @Ignore
+    @Test
+    public void load_spring() throws Exception {
+        String name = "info/jdip/legacy/saves/spring.jdip";
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream(name);
+        World legacyWorld = LegacyWorldLoader.readGameFile(in);
+        in = this.getClass().getClassLoader().getResourceAsStream(name);
+        boolean wordsAreEqual = WorldComparator.compareWorlds(legacyWorld, NewWorldLoader.importGameFile(in));
+        assertTrue(wordsAreEqual);
+    }
+
 }
